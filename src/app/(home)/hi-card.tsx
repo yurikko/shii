1c0a84b20adba963437496faf391187defa87e44
yurikko <52963221+yurikko@users.noreply.github.com>
2025-12-16 00:33:50 +0800

    Update greeting messages to Chinese

--- src/app/(home)/hi-card.tsx
+++ src/app/(home)/hi-card.tsx
@@ -7,13 +7,13 @@ function getGreeting() {
 	const hour = new Date().getHours()
 
 	if (hour >= 6 && hour < 12) {
-		return 'Good Morning'
+		return '早上好！☀️'
 	} else if (hour >= 12 && hour < 18) {
-		return 'Good Afternoon'
+		return '懒懒的下午〜🎐'
 	} else if (hour >= 18 && hour < 22) {
-		return 'Good Evening'
+		return '晚上好！🕶️'
 	} else {
-		return 'Good Night'
+		return '夜已经深啦🌙'
 	}
 }
 
@@ -32,7 +32,7 @@ export default function HiCard() {
 			<Card order={styles.order} width={styles.width} height={styles.height} x={x} y={y} className='text-center max-sm:static max-sm:translate-0'>
 				<img src='/images/avatar.png' className='mx-auto rounded-full' style={{ width: 120, height: 120, boxShadow: ' 0 16px 32px -5px #E2D9CE' }} />
 				<h1 className='font-averia mt-3 text-2xl'>
-					{greeting} <br /> I'm <span className='text-linear text-[32px]'>{username}</span> , Nice to <br /> meet you!
+					{greeting} <br /> <span className='text-linear text-[32px]'>{username}</span>今天也在努力地 <br /> 活着喵
 				</h1>
 			</Card>
 		</HomeDraggableLayer>
