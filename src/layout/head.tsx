c9c88373109a936ec1f25de98814f2376fd052eb
yurikko <52963221+yurikko@users.noreply.github.com>
2025-12-16 00:50:55 +0800

    Add additional stylesheets for Chinese fonts

--- src/layout/head.tsx
+++ src/layout/head.tsx
@@ -12,6 +12,10 @@ export default function Head() {
 			<link rel='preconnect' href='https://fonts.gstatic.cn' crossOrigin='anonymous' />
 
 			<link href='https://fonts.googleapis.cn/css2?family=Averia+Gruesa+Libre&display=swap' rel='stylesheet' />
+			<link rel='stylesheet' href='https://chinese-fonts-cdn.deno.dev/packages/syst/dist/SourceHanSerifCN/result.css' />
+			<link rel='stylesheet' href='https://chinese-fonts-cdn.deno.dev/packages/lxgwwenkaibright/dist/LXGWBright-Regular/result.css' />
+			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/firacode@6.2.0/distr/fira_code.css">
+			<link rel='stylesheet' href='https://chinese-fonts-cdn.deno.dev/packages/sypxzs/dist/%E6%80%9D%E6%BA%90%E5%B1%8F%E6%98%BE%E8%87%BB%E5%AE%8B/result.css' />
 
 			<Script src='https://www.googletagmanager.com/gtag/js?id=G-ZNSFR7C9PM' />
 			<Script id='google-analytics'>
